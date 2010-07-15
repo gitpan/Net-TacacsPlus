13d376e5ec7922987c6c0aad68e970aa823d76a3
Jozef Kutej <jozef@kutej.net>
2010-07-15 17:34:16 +0000

    Import of JKUTEJ/Net-TacacsPlus-1.09 from CPAN.
    gitpan-cpan-distribution: Net-TacacsPlus
    gitpan-cpan-version:      1.09
    gitpan-cpan-path:         JKUTEJ/Net-TacacsPlus-1.09.tar.gz
    gitpan-cpan-author:       JKUTEJ
    gitpan-cpan-maturity:     released

--- lib/Net/TacacsPlus.pm
+++ lib/Net/TacacsPlus.pm
@@ -20,7 +20,7 @@ Tacacs+ server (pap auth only) implemented by L<POE::Component::Server::TacacsPl
 
 =cut
 
-our $VERSION = '1.08';
+our $VERSION = '1.09';
 
 use strict;
 use warnings;
@@ -64,5 +64,13 @@ Authorization and Accounting contributed by Rubio Vaughan E<lt>rubio@passim.netE
 =head1 SEE ALSO
 
 tac-rfc.1.78.txt
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2006 by Jozef Kutej
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.8.4 or,
+at your option, any later version of Perl 5 you may have available.
 
 =cut
